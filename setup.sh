abe7b0e2f6f99407c02ac31f7523fe76b9ade3d4
iwamoto yuma <yuma@yumac.local>
2017-04-25 13:58:54 +0900

    fixed files

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@ sudo apt-get update
 wget https://raw.githubusercontent.com/openvswitch/ovs/master/utilities/ovs-docker
 chmod +x ovs-docker
 sudo cp ovs-docker /user/local/sbin/
-sudo apt-get install  linux-image-extra-$(uname -r)  linux-image-extra-virtual 
-sudo apt-get install  apt-transport-https  ca-certificates  curl software-properties-common 
+sudo apt-get install  linux-image-extra-$(uname -r)  linux-image-extra-virtual  -y
+sudo apt-get install  apt-transport-https  ca-certificates  curl software-properties-common  -y 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
